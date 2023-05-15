a9b773f3c44f215b6e9197b827b8ca5ef2942e13
rowpure <rowpure@gmail.com>
2023-05-15 23:03:29 +0900

    DB接続設定追加

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => 'db',
+	'username' => 'root',
+	'password' => 'root',
+	'database' => 'study_codeigniter',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
